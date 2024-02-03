13209ea7faca619a6f82ca8048c62ac72a8288ac
MacaronFR <denisft77@gmail.com>
2024-02-03 18:51:14 +0100

    Add mention in message + too large handling

--- src/main/kotlin/fr/imacaron/kaamelott/gif/Main.kt
+++ src/main/kotlin/fr/imacaron/kaamelott/gif/Main.kt
@@ -7,12 +7,15 @@ import dev.kord.core.Kord
 import dev.kord.core.behavior.interaction.response.DeferredPublicMessageInteractionResponseBehavior
 import dev.kord.core.behavior.interaction.response.respond
 import dev.kord.core.behavior.interaction.suggest
+import dev.kord.core.entity.User
 import dev.kord.core.event.interaction.AutoCompleteInteractionCreateEvent
 import dev.kord.core.event.interaction.GuildChatInputCommandInteractionCreateEvent
 import dev.kord.core.on
 import dev.kord.rest.builder.interaction.integer
 import dev.kord.rest.builder.interaction.string
 import dev.kord.rest.builder.message.addFile
+import dev.kord.rest.request.HttpStatus
+import dev.kord.rest.request.KtorRequestException
 import io.ktor.util.logging.*
 import org.slf4j.LoggerFactory
 import java.io.File
@@ -98,73 +101,92 @@ suspend fun main() {
         val user = interaction.user
         logger.debug("Receive command from ${user.effectiveName}. Defer it")
         val resp = interaction.deferPublicResponse()
+        val name = UUID.randomUUID()
         try {
             val epNum = interaction.command.integers["episode"]?.toByte() ?: run {
                 logger.debug("No ep num in command")
+                resp.respondBadCommand(user)
                 return@on
             }
             val book = interaction.command.integers["livre"]?.toByte() ?: run {
                 logger.debug("No book in command")
+                resp.respondBadCommand(user)
                 return@on
             }
             val ep = Episode(epNum, book)
             val time = try {
                 interaction.command.strings["timecode"]?.split(":")?.let {
                     if (it.size != 2) {
-                        resp.respondTimecode()
+                        resp.respondTimecode(user)
                         return@on
                     }
                     it[0].toInt() * 60 + it[1].toInt()
                 } ?: run {
                     logger.debug("No timecode in command")
+                    resp.respondBadCommand(user)
                     return@on
                 }
             } catch (e: NumberFormatException) {
-                resp.respondTimecode()
+                logger.debug("Time code not only numbers")
+                resp.respondTimecode(user)
                 return@on
             }
             if(time > ep.info.duration) {
-                resp.respondTropLoin(ep.info.duration, time)
+                logger.debug("Timecode greater than episode duration")
+                resp.respondTropLoin(ep.info.duration, time, user)
                 return@on
             }
             if(time < 0) {
-                resp.respondTropCourt()
+                logger.debug("Timecode less than 0")
+                resp.respondTropCourt(user)
                 return@on
             }
             val text = interaction.command.strings["text"] ?: run {
                 logger.debug("No text in command")
+                resp.respondBadCommand(user)
                 return@on
             }
             val scene = (ep.info.sceneChange.indexOfFirst { it > time } - 1).coerceAtLeast(0) + 1
             logger.debug("Getting scene $scene, starting at ${ep.getSceneStart(scene)} and last ${ep.getSceneDuration(scene)}")
             logger.debug("Creating meme")
-            ep.createMeme("${UUID.randomUUID()}", scene, text)
+            ep.createMeme("${name}", scene, text)
                 .onFailure {
                     logger.debug("Creating meme failed", it)
                     when(it) {
                         is NotEnoughTimeException -> {
                             logger.debug("Not enough time to create scene")
-                            resp.respond {
-                                content = "La portion choisie est trop petite, veuillez en sélectionner une autre"
-                            }
+                            resp.respondPortionTropCourte(user)
                         }
                         is ErrorWhileDrawingText -> {
                             logger.debug("Error while drawing text on scene")
-                            resp.respond {
-                                content = "Erreur lors du dessin du texte"
-                            }
+                            resp.respondTexteErreur(user)
                         }
                     }
                 }
                 .onSuccess {
                     logger.debug("meme $it successfully created")
                     resp.respond {
+                        content = "<@${user.id}>\n"
                         addFile(Path(it))
                     }
                 }
         }catch (e: Exception) {
-            logger.error(e)
-            resp.respondUnknownError()
+            when(e) {
+                is KtorRequestException -> {
+                    if(e.status.code == 413) {
+                        val size = File("gif/$name.gif").length()
+                        logger.warn("File $name.gif too large, ${size}B")
+                        resp.repondTropGros(user, size, "$name.gif")
+                    } else {
+                        logger.error(e)
+                        resp.respondUnknownError(user)
+                    }
+                }
+                else -> {
+                    logger.error(e)
+                    resp.respondUnknownError(user)
+                }
+            }
         }
     }
 
@@ -173,31 +195,69 @@ suspend fun main() {
     logger.info("Gracefully stopping")
 }
 
-suspend fun DeferredPublicMessageInteractionResponseBehavior.respondTimecode() {
+suspend fun DeferredPublicMessageInteractionResponseBehavior.respondTimecode(user: User) {
     respond {
-        content = "Le time code doit être sous la forme `mm:ss`"
+        content = "<@${user.id}>\nLe time code doit être sous la forme `mm:ss`"
         addFile(Path("pas_compliquer.gif"))
     }
 }
 
-suspend fun DeferredPublicMessageInteractionResponseBehavior.respondTropLoin(duration: Double, demand: Int) {
+suspend fun DeferredPublicMessageInteractionResponseBehavior.respondTropLoin(duration: Double, demand: Int, user: User) {
     respond {
         val min = duration.seconds.inWholeMinutes
-        content = "L'épisode fait ${min}min${duration.toInt()%60}s donc la scène à la ${demand/60}min et ${demand%60}s. Bon voilà quoi"
+        content = "<@${user.id}>\nL'épisode fait ${min}min${duration.toInt()%60}s donc la scène à la ${demand/60}min et ${demand%60}s. Bon voilà quoi"
         addFile(Path("pas_compliquer.gif"))
     }
 }
 
-suspend fun DeferredPublicMessageInteractionResponseBehavior.respondTropCourt() {
+suspend fun DeferredPublicMessageInteractionResponseBehavior.respondTropCourt(user: User) {
     respond {
-        content = "Euh l'épisode il commence à 00:00 pas avant"
+        content = "<@${user.id}>\nEuh l'épisode il commence à 00:00 pas avant"
         addFile(Path("pas_compliquer.gif"))
     }
 }
 
 
-suspend fun DeferredPublicMessageInteractionResponseBehavior.respondUnknownError() {
+suspend fun DeferredPublicMessageInteractionResponseBehavior.respondUnknownError(user: User) {
     respond {
-        content = "Une erreur est survenue"
+        content = "<@${user.id}>\nUne erreur est survenue"
+    }
+}
+
+suspend fun DeferredPublicMessageInteractionResponseBehavior.respondBadCommand(user: User) {
+    respond {
+        content = "<@${user.id}>\nNormalement c'est pas possible de faire ça"
+    }
+}
+
+suspend fun DeferredPublicMessageInteractionResponseBehavior.respondPortionTropCourte(user: User) {
+    respond {
+        content = "<@${user.id}>\nLa portion choisie est trop courte, veuillez en choisir une autre"
+    }
+}
+
+suspend fun DeferredPublicMessageInteractionResponseBehavior.respondTexteErreur(user: User) {
+    respond {
+        content = "<@${user.id}>\nErreur lors de l'ajout du texte"
+    }
+}
+
+suspend fun DeferredPublicMessageInteractionResponseBehavior.repondTropGros(user: User, fileSize: Long, fileName: String) {
+    respond {
+        var size = fileSize
+        var unit = 0
+        while(size > 1024) {
+            size /= 1024
+            unit++
+        }
+        val stringUnit = when(unit) {
+            0 -> "o"
+            1 -> "Ko"
+            2 -> "Mo"
+            3 -> "Go"
+            4 -> "To"
+            else -> "?"
+        }
+        content = "<@${user.id}>\nLe gif il est trop gros là\nFichier: `$fileName: $size$stringUnit`\nContactez macaron pour avoir le gif si vous le souhaitez"
     }
 }
