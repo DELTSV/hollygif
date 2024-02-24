f733e1518509631ac558fbd1ee7a0bba893febf2
MacaronFR <denisft77@gmail.com>
2024-02-24 22:54:35 +0100

    Start front

--- api/src/main/kotlin/fr/imacaron/gif/api/routing/Routing.kt
+++ api/src/main/kotlin/fr/imacaron/gif/api/routing/Routing.kt
@@ -3,10 +3,12 @@ package fr.imacaron.gif.api.routing
 import fr.imacaron.gif.api.routing.route.FileRoute
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.resources.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.io.File
 
 fun Application.configureRouting() {
 	install(Resources)
@@ -16,6 +18,7 @@ fun Application.configureRouting() {
 		}
 	}
 	routing {
+		staticFiles("/app", File("front"))
 		get("/") {
 			call.respondText("GIF API/APP")
 		}
