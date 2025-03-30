17d8957a313aaf8ffd46dcc835d442b6900d3860
MacaronFR <denisft77@gmail.com>
2025-03-30 19:46:15 +0200

    Fix cors ?

--- api/src/main/kotlin/fr/imacaron/gif/api/plugins/HTTP.kt
+++ api/src/main/kotlin/fr/imacaron/gif/api/plugins/HTTP.kt
@@ -14,6 +14,8 @@ fun Application.configureHTTP() {
 		allowMethod(HttpMethod.Put)
 		allowMethod(HttpMethod.Delete)
 		allowMethod(HttpMethod.Patch)
+		allowMethod(HttpMethod.Post)
+		allowMethod(HttpMethod.Get)
 		allowHeader(HttpHeaders.Authorization)
 		anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
 	}
